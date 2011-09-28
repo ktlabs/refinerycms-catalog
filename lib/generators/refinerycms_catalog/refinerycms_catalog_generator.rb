8aaca9ca2235e42e123551cdf6678adb64280068
mbashirov <ya.eskaflone@yandex.ru>
2011-09-29 02:53:45 +0400

    Added 1 second sleep to generator to make correct migration timestamps

--- lib/generators/refinerycms_catalog/refinerycms_catalog_generator.rb
+++ lib/generators/refinerycms_catalog/refinerycms_catalog_generator.rb
@@ -49,6 +49,7 @@ module ActiveRecord
       def self.next_migration_number(dirname) #:nodoc:
         next_migration_number = current_migration_number(dirname) + 1
         if ActiveRecord::Base.timestamped_migrations
+          sleep(1)
           [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
         else
           "%.3d" % next_migration_number
